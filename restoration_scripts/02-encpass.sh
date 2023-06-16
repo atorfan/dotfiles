0e67fc9f3082f61f53defd06a7928e4e10c0253e
alejandro.torres <alejandro.torres@holaluz.com>
2023-06-16 23:03:31 +0200

    encpass disabled in restoration scripts

--- restoration_scripts/02-encpass.sh
+++ restoration_scripts/02-encpass.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/ahnick/encpass.sh/master/encpass.sh -o ~/.local/bin/encpass.sh
-chmod go-rwx ~/.local/bin/encpass.sh
-chmod u+x ~/.local/bin/encpass.sh
+#curl https://raw.githubusercontent.com/ahnick/encpass.sh/master/encpass.sh -o ~/.local/bin/encpass.sh
+#chmod go-rwx ~/.local/bin/encpass.sh
+#chmod u+x ~/.local/bin/encpass.sh
--- restoration_scripts/03-password-store.sh
+++ restoration_scripts/03-password-store.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-apt install pass -y
+#apt install pass -y
 
-gpg --batch --pinentry-mode=loopback --yes --passphrase "H0l4Luz22" --always-trust --no-use-agent --quick-generate-key 'alejandro.torres@holaluz.com_TEST'
+#gpg --batch --pinentry-mode=loopback --yes --passphrase "H0l4Luz22" --always-trust --no-use-agent --quick-generate-key 'alejandro.torres@holaluz.com_TEST'
 
-pass init 'alejandro.torres@holaluz.com_TEST'
+#pass init 'alejandro.torres@holaluz.com_TEST'
